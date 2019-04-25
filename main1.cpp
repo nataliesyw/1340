36ecd914eb61042599e9868ef58a70746fdaf324
scarlettso <47234834+scarlettso@users.noreply.github.com>
2019-04-26 01:52:56 +0800

    Sort command-full version

--- main1.cpp
+++ main1.cpp
@@ -287,6 +287,7 @@ int search(string attribute, company_struct sys[], int n){
 void sort_record(string attribute, string order, company_struct sys[], int num){
   int i, j ,idx;
   string min;
+  string max;
   // selection sort
 
   // sort by ascending order
@@ -385,7 +386,7 @@ void sort_record(string attribute, string order, company_struct sys[], int num){
           {
               if (sys[j].early_leave_count < min_int)
               {
-                  min = sys[j].early_leave_count;
+                  min_int = sys[j].early_leave_count;
                   idx = j;
               }
           }
@@ -412,7 +413,7 @@ void sort_record(string attribute, string order, company_struct sys[], int num){
           {
               if (sys[j].attendance_count < min_int)
               {
-                  min = sys[j].attendance_count;
+                  min_int = sys[j].attendance_count;
                   idx = j;
               }
           }
@@ -429,7 +430,147 @@ void sort_record(string attribute, string order, company_struct sys[], int num){
     }
     
   }
-        
+
+  // sort by descending order
+  if (order == "d"){
+
+    // sort by age
+    if (attribute == "Age"){
+      for (i = 0; i < num - 1; i++)
+      {
+          max = sys[i].age;
+          idx = i;
+          
+          for (j = i + 1; j < num; j++)
+          {
+              if (sys[j].age < max)
+              {
+                  max = sys[j].age;
+                  idx = j;
+              }
+          }
+          
+          if (idx != i)
+          {
+              company_struct temp;
+              // swap pb[i] & pb[idx]
+              temp    = sys[i];
+              sys[i]   = sys[idx];
+              sys[idx] = temp;           
+          }
+      }
+    }
+
+    // sort by salary
+    if (attribute == "Salary"){
+      for (i = 0; i < num - 1; i++)
+      {
+          max = sys[i].salary;
+          idx = i;
+          
+          for (j = i + 1; j < num; j++)
+          {
+              if (sys[j].salary < max)
+              {
+                  max = sys[j].salary;
+                  idx = j;
+              }
+          }
+          
+          if (idx != i)
+          {
+              company_struct temp;
+              // swap pb[i] & pb[idx]
+              temp    = sys[i];
+              sys[i]   = sys[idx];
+              sys[idx] = temp;           
+          }
+      }
+    }
+
+    int max_int;          // minimum for interger
+    // sort by times for being late
+    if (attribute == "Late"){
+      for (i = 0; i < num - 1; i++)
+      {
+          max_int = sys[i].late_count;
+          idx = i;
+          
+          for (j = i + 1; j < num; j++)
+          {
+              if (sys[j].late_count < max_int)
+              {
+                  max_int = sys[j].late_count;
+                  idx = j;
+              }
+          }
+          
+          if (idx != i)
+          {
+              company_struct temp;
+              // swap pb[i] & pb[idx]
+              temp    = sys[i];
+              sys[i]   = sys[idx];
+              sys[idx] = temp;           
+          }
+      }
+    }
+
+    // sort by times for left early
+    if (attribute == "Early leave"){
+      for (i = 0; i < num - 1; i++)
+      {
+          max_int = sys[i].early_leave_count;
+          idx = i;
+          
+          for (j = i + 1; j < num; j++)
+          {
+              if (sys[j].early_leave_count < max_int)
+              {
+                  max_int = sys[j].early_leave_count;
+                  idx = j;
+              }
+          }
+          
+          if (idx != i)
+          {
+              company_struct temp;
+              // swap pb[i] & pb[idx]
+              temp    = sys[i];
+              sys[i]   = sys[idx];
+              sys[idx] = temp;           
+          }
+      }
+    }
+
+    //sort by attdence
+    if (attribute == "Attendence"){
+      for (i = 0; i < num - 1; i++)
+      {
+          max = sys[i].attendance_count;
+          idx = i;
+          
+          for (j = i + 1; j < num; j++)
+          {
+              if (sys[j].attendance_count < max_int)
+              {
+                  max_int = sys[j].attendance_count;
+                  idx = j;
+              }
+          }
+          
+          if (idx != i)
+          {
+              company_struct temp;
+              // swap pb[i] & pb[idx]
+              temp    = sys[i];
+              sys[i]   = sys[idx];
+              sys[idx] = temp;           
+          }
+      }
+    }
+    
+  }       
   for ( int k = 0; k < num; k++){
     cout << "Name:\t" << sys[k].name << endl;
     cout << "ID:\t" << sys[k].id << endl;
